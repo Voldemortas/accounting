ede97e93ed7dd51b6012ace6f9262b8d486c4eba
Andrius Simanaitis <eragonas5@gmail.com>
2021-01-04 17:52:37 +0200

    added pdf

--- src/Bill.vue
+++ src/Bill.vue
@@ -2,13 +2,14 @@
   <div id="app">
     <main class="main">
       <PreviewTable v-bind:data="data" /><br />
-      <a v-bind:href="`${path}/`">Go Back</a>
+      <a v-bind:href="`${path}/`">Go Back</a> <a href="#" @click="getPDF">Get PDF</a>
     </main>
   </div>
 </template>
 
 <script>
 import PreviewTable from './components/PreviewTable.vue'
+import html2pdf from 'html2pdf.js'
 
 export default {
   name: 'App',
@@ -21,5 +22,18 @@ export default {
   data() {
     return { data: JSON.parse(localStorage.getItem('products')) }
   },
+  methods: {
+    getPDF(){
+      const prev = document.getElementsByTagName('html')[0].innerHTML.toString()
+      document.body.innerHTML+='<div style="position: absolute; top: 500px; left: calc(50% - 34px)">andriaus.lt</div>'
+      html2pdf(        
+        document.getElementsByTagName('html')[0].innerHTML.toString().replaceAll(/<a href="\/">Go Back<\/a>\s*\n*<a href="#">Get PDF<\/a>/g, ''),
+        {
+          filename: 'receipt.pdf',}
+      )
+      document.body.innerHTML = prev
+    }
+  }
 }
+//
 </script>
